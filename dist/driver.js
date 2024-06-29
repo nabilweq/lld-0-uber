289e2888fb0209faf3302e3dee24daefcb8fc6ab
nabilweq <nabeeltkanr@gmail.com>
2024-06-29 13:39:01 +0530

    updations

--- dist/driver.js
+++ dist/driver.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Driver = void 0;
 const common_1 = require("./common");
 class Driver {
-    constructor(id, name, rating, status = common_1.Status.BOOKED) {
+    constructor(id, name, rating, status = common_1.Status.IDLE) {
         this.id = id;
         this.name = name;
         this.rating = rating;
