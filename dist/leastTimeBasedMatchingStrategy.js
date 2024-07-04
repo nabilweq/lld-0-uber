14e98293e1d7a80ab457d82b691beef8e254b293
Muhammed Nabeel T K <62200954+nabilweq@users.noreply.github.com>
2024-07-04 11:17:45 +0530

    update:deleted dist directory

--- dist/leastTimeBasedMatchingStrategy.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LeastTimeBasedMatchingStrategy = void 0;
-class LeastTimeBasedMatchingStrategy {
-    findDriver(drivers, location) {
-        return drivers.length > 0 ? drivers[0] : null;
-    }
-}
-exports.LeastTimeBasedMatchingStrategy = LeastTimeBasedMatchingStrategy;
